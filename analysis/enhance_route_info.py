a5c63ca9d5c89e9ee2b7a22872e3a5d2d81cbbf0
Your Name <charlie.c@berkeley.edu>
2025-08-28 13:46:52 -0700

    new css
    Former-commit-id: 8884745bba2a32355d5de48c4e8209677f6aa368

---
+++ analysis/enhance_route_info.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
+static_output_dir = os.environ.get("STATIC_MUNI_DATA")
+data_dir = Path(static_output_dir) / "data"
+routes_csv = Path(data_dir) / "routes.txt"
+
+df = pd.read_csv(routes_csv)
+trimmed_df = df.loc[df["agency_id"] == "SF", ["route_short_name", "route_long_name", "route_color"]]
+print(trimmed_df)
+trimmed_df.to_csv(f"{Path(static_output_dir) / 'route_info.csv'}", index=False)
